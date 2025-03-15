6c9832aab660bbfe9242e7b0eaaedc9a938738d1
cgrazy <66645171+cgrazy@users.noreply.github.com>
2025-03-15 22:34:57 +0100

    Add first parameter handling

--- Program.cs
+++ Program.cs
@@ -10,28 +10,74 @@ namespace EDIDChecker
         {
             Console.WriteLine("EDID Checker");
 
-            //string edid1_Customer =
-            //        "00FFFFFFFFFFFF0010AC5D4157353946101E010380351E78EEEE95A3544C99260F5054A54B00714F8180A9C0D1C00101010101010101023A801871382D40582C45000F282100001E000000FF00324A38435631330A2020202020000000FC0044454C4C205532343139480A20000000FD00384C1E5311000A2020202020200126";
+            string fileName=string.Empty;
+            string edid=string.Empty;
+            
+            if (args.Length>=1)
+            {
+                if(string.Compare(args[0].ToUpperInvariant(),"-F")==0)
+                {
+                    if(args.Length!=2)
+                    {
+                        Console.WriteLine($"No file name specified. Abort!");
+                        return;
+                    }
 
-            string edid2_Customer =
-                    "00FFFFFFFFFFFF00371801000100000004190103807341780ACF74A3574CB02309484C2108008180950090408100B300A940D1008140023A801871382D40582C450010090000001E011D8018711C1620582C2500C48E2100009E000000FC004D58582D412D320A2020202020000000FD00163E0E5010000A2020202020200190";
+                    fileName = args[1];
 
-            //string edid3_Customer =
-            //        "00FFFFFFFFFFFF00371801000100000004190103807341780ACF74A3574CB02309484C2108008180950090408100B300A940D1008140023A801871382D40582C450010090000001E011D8018711C1620582C2500C48E2100009E000000FC004D58582D412D320A2020202020000000FD00163E0E5010000A2020202020200190";
+                    if(!File.Exists(fileName))
+                    {
+                        Console.WriteLine($"The file '{fileName}' doesn't exists.");
+                        return;
+                    }
+                    else
+                    {
+                        if(!string.IsNullOrEmpty(fileName))
+                        {
+                            edid = File.ReadAllText(fileName).Replace(" ","");
+                        }
+                    }
+                }
 
-            string edid_Benq = "00ffffffffffff0009d12580455400000e1e0103804628782e87d1a8554d9f250e5054a56b80818081c08100a9c0b300d1c001010101023a801871382d40582c4500ffff0000001e000000ff0033344c30303633383031390a20000000fd00324c1e8c24000a202020202020000000fc0042656e5120504433323030550a017a020333f15161605f5e5d101f2221200514041312030123090707830100006c030c001000183c200040010267d85dc40178880104740030f2705a80b0588a00c48f2100001e565e00a0a0a029502f203500c48f2100001a000000000000000000000000000000000000000000000000000000000000000000000000000000004a";
+                if(string.Compare(args[0].ToUpperInvariant(),"-E")==0)
+                {
+                    if(args.Length!=2)
+                    {
+                        Console.WriteLine($"No edid information specified. Abort!");
+                        return;
+                    }
+
+                    edid = args[1];
+                }
+            }
+
+            if(string.IsNullOrWhiteSpace(edid))
+            {
+                Console.WriteLine($"No edid '{edid}' given. Abort!");
+                return;
+            }
+
+            if(!IsValidEDID(edid))
+            {
+                Console.WriteLine($"No valid edid given: '{edid}'. Abort!");
+                return;
+            }
 
             var size = new Size(1920,1080);
-            CheckEdid(edid2_Customer, size);
+            CheckEdid(edid, size);
+        }
 
-            size = new Size(1920,1080);
-            CheckEdid(edid_Benq, size);
+        static bool IsValidEDID(string edid)
+        {
+            bool isValid = false;
 
-            size = new Size(3840,2160);
-            CheckEdid(edid_Benq, size, 1);
-            CheckEdid(edid_Benq, size, 2);
-            CheckEdid(edid_Benq, size, 3);
-            CheckEdid(edid_Benq, size, 4);
+            edid=edid.ToUpperInvariant();
+            if(edid.StartsWith("00FFFFFFFFFFFF00"))
+            {
+                isValid = true;
+            }
+
+            return isValid;
         }
 
         static void CheckEdid(string edid, Size size)
